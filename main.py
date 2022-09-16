69177b526709f614e93c62884f639d7503139dd9
HoganHicks-10 <830511@mystma.org>
2022-09-16 14:39:31 -0500

    2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def topFIveMovies(a, b, c, d, e,):
+def topFiveMovies(a, b, c, d, e,):
     return a +" " + b +" " + c + " " + d + " " + e 
     print(topFiveMovies("shrek 1 ", "shrek 2 ", "shrek 3 ", "shrek 4 ", "shrek 5"))
 
