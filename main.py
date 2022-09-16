fccf72564e665fc5be383975b29f5f1a729beb01
HoganHicks-10 <830511@mystma.org>
2022-09-16 14:38:19 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-#WRITE YOUR CODE IN THIS FILE
+def topFIveMovies(a, b, c, d, e,):
+    return a +" " + b +" " + c + " " + d + " " + e 
+    print(topFiveMovies("shrek 1 ", "shrek 2 ", "shrek 3 ", "shrek 4 ", "shrek 5"))
+
